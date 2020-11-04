1d7a1ffb93dfba1757d619520550f5ce9eff64f6
Vins_DEV <69247128+VincentsSaerang@users.noreply.github.com>
2020-11-04 06:29:24 -0600

    Fix help.js

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -3,6 +3,7 @@ const { MessageEmbed } = require('discord.js');
 exports.run = async (client, message, args, color) => {
   if (!args[0]) {
     let module = client.helps.array();
+    
     const embed = new MessageEmbed()
     .setAuthor(client.user.username + ' Category', client.user.displayAvatarURL())
     .setColor(color)
@@ -13,13 +14,16 @@ exports.run = async (client, message, args, color) => {
     return message.channel.send(embed);
   } else {
     let cmd = args[0]
-    if (client.helps.get(cmd).names) {
+    if (client.helps.get(cmd).call) {
       const embed = new MessageEmbed()
       .setAuthor(`${client.helps.get(cmd).name} Category`, client.user.displayAvatarURL())
       .setColor(color)
-      for (const x of client.helps.array(cmd)) {
-        embed.setDescription(x.cmds.map(a => `\`${client.prefix}${a.name}\`\n${a.description}`).join("\n\n"))
-      }
+      .setThumbnail(client.user.displayAvatarURL())
+      
+      const x = client.helps.get(cmd);
+        let index = 0;
+        embed.setDescription(x.cmds.map(a => `\`${++index}\` **|** \`${client.prefix}${a.usage}\`\n${a.description}`).join("\n\n"))
+      
       return message.channel.send(embed)
     }
   }
